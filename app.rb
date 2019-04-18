0106bf7f2400439c1eda239e5af6327cc4961b56
Alison Reilly <reillyamr@gmail.com>
2019-04-18 15:01:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is__"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is__"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song__"
+  end 
 end
